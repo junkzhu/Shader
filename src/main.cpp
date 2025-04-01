36bb2816a6783dd5931daffd409c2d9c5f21ead6
rookiebrianz <rookiebrianz@gmail.com>
2025-04-01 19:39:07 +0800

    添加imgui的库

--- src/main.cpp
+++ src/main.cpp
@@ -39,9 +39,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
+    #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    #endif
 
     // glfw window creation
     // --------------------
